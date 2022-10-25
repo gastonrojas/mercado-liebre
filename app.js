94295704c85d6efbdfad01a9a92bfd7a6c59c074
tonga <grojas89@hotmail.com>
2022-10-25 15:25:00 -0300

    agrego register y login con estilos. modifico index

--- app.js
+++ app.js
@@ -9,4 +9,21 @@ app.get('/', (req, res)=>{
     res.sendFile(path.resolve('./views/home.html'))
 })
 
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve('./views/register.html'))
+})
+
+app.post('/register',(req, res)=>{   
+    res.redirect('/')
+})
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve('./views/login.html'))
+})
+
+app.post('/login',(req, res)=>{   
+    res.redirect('/')
+})
+
+                  
 const connectedServer = app.listen(3000, ()=> console.log(`Servidor express escuchando en el puerto ${connectedServer.address().port}`))
